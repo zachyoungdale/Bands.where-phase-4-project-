453fc10219e80ea1a19d6f6025d168fcfa7add7e
Briana <briana_brewster4@yahoo.com>
2022-11-15 11:30:30 -0600

    added frontend login form

---
+++ client/src/components/App.js
@@ -0,0 +1,42 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Login from './Login';
+import React, { useState, useEffect } from "react";
+
+function App() {
+
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    fetch("/me").then((response) => {
+      if (response.ok) {
+        response.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (user) {
+    return <h2>Welcome, {user.username}!</h2>;
+  } else {
+    return <Login onLogin={setUser} />;
+  }
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //      <Login/>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+}
+
+export default App;
