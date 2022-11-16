df89953a1d0e4714f1de7b9d9656a6ff23e6896f
Zachary Youngdale <zyoungdale@gmail.com>
2022-11-16 16:41:35 -0500

    Concert button now toggles

--- client/src/ConcertCard.js
+++ client/src/ConcertCard.js
@@ -1,48 +1,60 @@
-import Moment from 'react-moment'
-
-function ConcertCard({setUsersConcerts, id, venue, datetime, url, artist, artistImage, concert}) {
+import Moment from "react-moment";
+import { useState } from "react";
+function ConcertCard({
+  setUsersConcerts,
+  id,
+  venue,
+  datetime,
+  url,
+  artist,
+  artistImage,
+  concert,
+}) {
+  const [buttonClick, setButtonClick] = useState(false);
 
   function handleGoing(concertObj) {
-    setUsersConcerts(prev => [...prev, concertObj])
+    setButtonClick((prev) => !prev);
+    setUsersConcerts((prev) => [...prev, concertObj]);
   }
-        
-    function dateAndLinks(){
-        if (Array.isArray(datetime)) {
-            return (
-                <div>
-                <h4>Showings</h4>
-                {datetime.map((datetime, i) => {
-                    return (
-                        <div>
-                            <Moment date={datetime} format="LLLL"/>
-                            <br></br>
-                            <a href={url[i]}>Buy Tickets</a>
-                        </div>
-                    )
-                })}
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <Moment date={datetime} format="LLLL"/>
-                    <br></br>
-                    <a href={url}>Buy Tickets</a>
-                </div>
-            )
-        }
-    }
 
-    return (
+  function dateAndLinks() {
+    if (Array.isArray(datetime)) {
+      return (
         <div>
-            <img alt={artist} src={artistImage}></img>
-            <p>{artist}</p>
-            <p>at {venue}</p>
-            {dateAndLinks()}
-            <button onClick={(e) => handleGoing(concert)}>I'm Going to This Concert!</button>
+          <h4>Showings</h4>
+          {datetime.map((datetime, i) => {
+            return (
+              <div>
+                <Moment date={datetime} format="LLLL" />
+                <br></br>
+                <a href={url[i]}>Buy Tickets</a>
+              </div>
+            );
+          })}
         </div>
-    )
+      );
+    } else {
+      return (
+        <div>
+          <Moment date={datetime} format="LLLL" />
+          <br></br>
+          <a href={url}>Buy Tickets</a>
+        </div>
+      );
+    }
+  }
 
+  return (
+    <div>
+      <img alt={artist} src={artistImage}></img>
+      <p>{artist}</p>
+      <p>at {venue}</p>
+      {dateAndLinks()}
+      <button onClick={(e) => handleGoing(concert)}>
+        {buttonClick ? "Im Going To This Concert!" : "Attending?"}
+      </button>
+    </div>
+  );
 }
 
 export default ConcertCard;
