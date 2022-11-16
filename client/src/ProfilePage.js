753ed466798d7d2c6f1d75333ad5dc5aa2f10e80
Zachary Youngdale <zyoungdale@gmail.com>
2022-11-16 11:56:52 -0500

    added profile picture to profile page

--- client/src/ProfilePage.js
+++ client/src/ProfilePage.js
@@ -1,6 +1,9 @@
 function ProfilePage({ spotifyUser, spotifyArtists }) {
+  console.log(spotifyUser);
+
   return (
     <div>
+      <img src={spotifyUser?.images ? spotifyUser?.images[0].url : null}></img>
       <h1>{spotifyUser?.display_name}</h1>
       <a href={spotifyUser?.external_urls?.spotify}>Spotify Account</a>
       <h3>Top Artists:</h3>
